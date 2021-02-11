3239c351d1f107f2d548d6c1ec5b6f7825922749
sonalk878 <sonalkumar878@gmail.com>
2021-02-11 12:08:53 -0500

    trail 11

--- my-second-game/js/main.js
+++ my-second-game/js/main.js
@@ -40,9 +40,9 @@ function create() {
     this.add.image(400, 300, 'sky');
 
     gemAudio = this.sound.add("gemAudio", { loop: false });
-    this.physics.add.collider(this.dude, this.star, function (dude, star) {
+   /* this.physics.add.collider(this.dude, this.star, function (dude, star) {
         gemAudio.play();
-    });
+    });*/
 
     platforms = this.physics.add.staticGroup();
 
