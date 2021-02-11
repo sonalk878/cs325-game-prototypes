cb07255963317157080661b29639f3f6b1e5fa86
sonalk878 <sonalkumar878@gmail.com>
2021-02-10 23:18:00 -0500

    trail 5

--- my-second-game/js/main.js
+++ my-second-game/js/main.js
@@ -39,7 +39,8 @@ function preload() {
 function create() {
     this.add.image(400, 300, 'sky');
     
-
+    music = this.add.audio('gemAudio');
+    music.play();
     platforms = this.physics.add.staticGroup();
 
     platforms.create(400, 568, 'ground').setScale(2).refreshBody();
@@ -119,8 +120,8 @@ function update() {
 
 function collectStar(player, star) {
     star.disableBody(true, true);
-    music = this.add.audio('gemAudio');
-    music.play();
+    
+  //  music.stop();
     //let soundSample = this.sound.add('gemAudio');
     //soundSample.play();
     score += 10;
