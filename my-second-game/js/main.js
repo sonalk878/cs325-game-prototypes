8c2c2da8709ca2456bbbf65042d009e6e83aeb36
sonalk878 <sonalkumar878@gmail.com>
2021-02-10 22:41:45 -0500

    trail 3

--- my-second-game/js/main.js
+++ my-second-game/js/main.js
@@ -1,134 +1,122 @@
 import "./phaser.js";
-// Linted with standardJS - https://standardjs.com/
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
 
-// Initialize the Phaser Game object and set default game window size
-const game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
-    preload: preload,
-    create: create,
-    update: update
-})
+var player;
+var stars;
+var platforms;
+var cursors;
+var score = 0;
+var scoreText;
 
-// Declare shared variables at the top so all methods can access them
-let score = 0
-let scoreText
-let platforms
-let diamonds
-let cursors
-let player
+var game = new Phaser.Game(config);
 
 function preload() {
-    // Load & Define our game assets
-    game.load.image('sky', './assets/sky.png')
-    game.load.image('ground', './assets/platform.png')
-    game.load.image('diamond', './assets/diamond.png')
-    game.load.spritesheet('woof', './assets/woof.png', 32, 32)
+    this.load.image('sky', './assets/sky.png');
+    this.load.image('ground', './assets/platform.png');
+    this.load.image('star', './assets/star.png');
+    this.load.image('bomb', './assets/bomb.png');
+    this.load.spritesheet('dude', './assets/dude.png', { frameWidth: 32, frameHeight: 48 });
 }
 
 function create() {
-    //  We're going to be using physics, so enable the Arcade Physics system
-    game.physics.startSystem(Phaser.Physics.ARCADE)
+    this.add.image(400, 300, 'sky');
 
-    //  A simple background for our game
-    game.add.sprite(0, 0, 'sky')
+    platforms = this.physics.add.staticGroup();
 
-    //  The platforms group contains the ground and the 2 ledges we can jump on
-    platforms = game.add.group()
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
 
-    //  We will enable physics for any object that is created in this group
-    platforms.enableBody = true
+    platforms.create(600, 400, 'ground');
+    platforms.create(50, 250, 'ground');
+    platforms.create(750, 220, 'ground');
 
-    // Here we create the ground.
-    const ground = platforms.create(0, game.world.height - 64, 'ground')
+    player = this.physics.add.sprite(100, 450, 'dude');
 
-    //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
-    ground.scale.setTo(2, 2)
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
 
-    //  This stops it from falling away when you jump on it
-    ground.body.immovable = true
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
 
-    //  Now let's create two ledges
-    let ledge = platforms.create(400, 450, 'ground')
-    ledge.body.immovable = true
+    this.anims.create({
+        key: 'turn',
+        frames: [{ key: 'dude', frame: 4 }],
+        frameRate: 20
+    });
 
-    ledge = platforms.create(-75, 350, 'ground')
-    ledge.body.immovable = true
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
 
-    // The player and its settings
-    player = game.add.sprite(32, game.world.height - 150, 'woof')
+    cursors = this.input.keyboard.createCursorKeys();
 
-    //  We need to enable physics on the player
-    game.physics.arcade.enable(player)
+    stars = this.physics.add.group({
+        key: 'star',
+        repeat: 11,
+        setXY: { x: 12, y: 0, stepX: 70 }
+    });
 
-    //  Player physics properties. Give the little guy a slight bounce.
-    player.body.bounce.y = 0.2
-    player.body.gravity.y = 800
-    player.body.collideWorldBounds = true
+    stars.children.iterate(function (child) {
 
-    //  Our two animations, walking left and right.
-    player.animations.add('left', [0, 1], 10, true)
-    player.animations.add('right', [2, 3], 10, true)
+        child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
 
-    //  Finally some diamonds to collect
-    diamonds = game.add.group()
+    });
 
-    //  Enable physics for any object that is created in this group
-    diamonds.enableBody = true
+    scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
 
-    //  Create 12 diamonds evenly spaced apart
-    for (var i = 0; i < 12; i++) {
-        const diamond = diamonds.create(i * 70, 0, 'diamond')
+    this.physics.add.collider(player, platforms);
+    this.physics.add.collider(stars, platforms);
 
-        //  Drop em from the sky and bounce a bit
-        diamond.body.gravity.y = 1000
-        diamond.body.bounce.y = 0.3 + Math.random() * 0.2
-    }
-
-    //  Create the score text
-    scoreText = game.add.text(16, 16, '', { fontSize: '32px', fill: '#000' })
-
-    //  And bootstrap our controls
-    cursors = game.input.keyboard.createCursorKeys()
+    this.physics.add.overlap(player, stars, collectStar, null, this);
 }
 
 function update() {
-    //  We want the player to stop when not moving
-    player.body.velocity.x = 0
-
-    //  Setup collisions for the player, diamonds, and our platforms
-    game.physics.arcade.collide(player, platforms)
-    game.physics.arcade.collide(diamonds, platforms)
-
-    //  Call callectionDiamond() if player overlaps with a diamond
-    game.physics.arcade.overlap(player, diamonds, collectDiamond, null, this)
-
-    // Configure the controls!
     if (cursors.left.isDown) {
-        player.body.velocity.x = -150
-        player.animations.play('left')
-    } else if (cursors.right.isDown) {
-        player.body.velocity.x = 150
-        player.animations.play('right')
-    } else {
-        // If no movement keys are pressed, stop the player
-        player.animations.stop()
+        player.setVelocityX(-160);
+
+        player.anims.play('left', true);
+    }
+    else if (cursors.right.isDown) {
+        player.setVelocityX(160);
+
+        player.anims.play('right', true);
+    }
+    else {
+        player.setVelocityX(0);
+
+        player.anims.play('turn');
     }
 
-    //  This allows the player to jump!
     if (cursors.up.isDown && player.body.touching.down) {
-        player.body.velocity.y = -400
-    }
-    // Show an alert modal when score reaches 120
-    if (score === 120) {
-        alert('You win!')
-        score = 0
+        player.setVelocityY(-330);
     }
 }
 
-function collectDiamond(player, diamond) {
-    // Removes the diamond from the screen
-    diamond.kill()
+function collectStar(player, star) {
+    star.disableBody(true, true);
 
-    //  And update the score
-    score += 10
-    scoreText.text = 'Score: ' + score
+    score += 10;
+    scoreText.setText('Score: ' + score);
 }
